edad66007364cf03c2b257d9d6964ba49115bfd7
YashRajRao <yashrajrao22@gmail.com>
2024-05-29 00:24:38 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import About from "./Pages/About";
+import { Profile } from "./Pages/Profile";
 
 function App() {
-
   return (
     <>
-      <h1 className='text-red-500'>Hello
-      </h1>
-       
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
